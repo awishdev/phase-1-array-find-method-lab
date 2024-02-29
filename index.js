4f6474748536011865370822b40e794e21fab194
Arthur Ish <arthurishdev@gmail.com>
2024-02-28 23:52:10 -0800

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function isWin(element){
+
+    return (element.result === 'W');
+
+}
+
+function superbowlWin(dataRecord){
+    
+    if (dataRecord.find(isWin) !== undefined){
+        return dataRecord.find(isWin).year;
+    }
+    else{
+        return undefined;
+    }
+
+
+}
